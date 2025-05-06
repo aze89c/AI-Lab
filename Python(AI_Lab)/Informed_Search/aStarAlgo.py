f1d59b79a8ed0141a3e7ec97ac69decfe379c782
aze89c <azizbharmal07@gmail.com>
2025-05-06 10:06:58 +0530

    Update aStarAlgo.py

--- Python(AI_Lab)/Informed_Search/aStarAlgo.py
+++ Python(AI_Lab)/Informed_Search/aStarAlgo.py
@@ -41,123 +41,94 @@ it is not practical for various large-scale problems.
  
  
 def aStarAlgo(start_node, stop_node): 
- 
-    open_set = set(start_node) 
-    closed_set = set() 
-    g = {} #store distance from starting node 
-    parents = {}# parents contains an adjacency map of all nodes 
- 
-#ditance of starting node from itself is zero 
-    g[start_node] = 0 
-#start_node is root node i.e it has no parent nodes 
-#so start_node is set to its own parent node 
-    parents[start_node] = start_node 
- 
- 
+    open_set = set([start_node])  # Set of nodes to be evaluated
+    closed_set = set()  # Set of nodes already evaluated
+    g = {}  # Store distance from the start node
+    parents = {}  # Stores the parent of each node
+
+    # Distance of start_node from itself is zero
+    g[start_node] = 0
+
+    # Start_node is its own parent
+    parents[start_node] = None
+
     while len(open_set) > 0: 
         n = None 
- 
-#node with lowest f() is found 
-    for v in open_set: 
-       if n == None or g[v] + heuristic(v) < g[n] + heuristic(n): 
-            n = v 
- 
- 
-            if n == stop_node or Graph_nodes[n] == None: 
-                pass 
-            else: 
-                for (m, weight) in get_neighbors(n): 
-#nodes 'm' not in first and last set are added to first 
-#n is set its parent 
-                    if m not in open_set and m not in closed_set: 
-                        open_set.add(m) 
- 
- 
-            parents[m] = n 
-            g[m] = g[n] + weight 
- 
- 
-#for each node m,compare its distance from start i.e g(m) to the 
-#from start through n node 
-    else: 
-        if g[m] > g[n] + weight: 
-#update g(m) 
-            g[m] = g[n] + weight 
-#change parent of m to n 
-            parents[m] = n 
- 
-#if m in closed set,remove and add to open 
-        if m in closed_set: 
-            closed_set.remove(m) 
-            open_set.add(m) 
- 
-        if n == None: 
-            print('Path does not exist!') 
-            return None 
- 
-# if the current node is the stop_node 
-# then we begin reconstructin the path from it to the start_node 
+
+        # Find the node with the lowest f(n) = g(n) + h(n)
+        for node in open_set: 
+            if n is None or g[node] + heuristic(node) < g[n] + heuristic(n): 
+                n = node
+
+        # If no node is found, path does not exist
+        if n is None:
+            print('Path does not exist!')
+            return None
+
+        # If the goal node is reached, reconstruct the path
         if n == stop_node: 
             path = [] 
- 
-        while parents[n] != n: 
-            path.append(n) 
-            n = parents[n] 
- 
-        path.append(start_node) 
- 
-        path.reverse() 
- 
- 
- 
-  
-    print('Path found: {}'.format(path)) 
-    return path 
- 
- 
-# remove n from the open_list, and add it to closed_list 
-# because all of his neighbors were inspected 
-    open_set.remove(n) 
-    closed_set.add(n) 
- 
-    print('Path does not exist!') 
+            while n is not None: 
+                path.append(n) 
+                n = parents[n] 
+            path.reverse() 
+            print('Path found: {}'.format(path)) 
+            return path 
+
+        # For all neighbors of the current node
+        for (neighbor, weight) in get_neighbors(n): 
+            if neighbor not in open_set and neighbor not in closed_set: 
+                open_set.add(neighbor) 
+                parents[neighbor] = n 
+                g[neighbor] = g[n] + weight 
+            else: 
+                # Update g(neighbor) if a shorter path is found
+                if g[neighbor] > g[n] + weight: 
+                    g[neighbor] = g[n] + weight 
+                    parents[neighbor] = n 
+
+                    # If the neighbor is in closed_set, move it to open_set
+                    if neighbor in closed_set: 
+                        closed_set.remove(neighbor) 
+                        open_set.add(neighbor) 
+
+        # Remove n from open_set and add it to closed_set
+        open_set.remove(n) 
+        closed_set.add(n) 
+
+    print('Path does not exist!')
     return None 
- 
-#define fuction to return neighbor and its distance 
-#from the passed node 
+
+# Define function to return neighbors and their distances
 def get_neighbors(v): 
     if v in Graph_nodes: 
         return Graph_nodes[v] 
     else: 
-        return None 
-#for simplicity we ll consider heuristic distances given 
-#and this function returns heuristic distance for all nodes 
+        return []
+
+# Heuristic function (predefined heuristic distances)
 def heuristic(n): 
     H_dist = { 
-'A': 11, 
-'B': 6, 
-'C': 99, 
-'D': 1, 
-'E': 7, 
-'G': 0, 
- 
-} 
- 
-    return H_dist[n] 
- 
- 
- 
-#Describe your graph here 
+        'A': 11, 
+        'B': 6, 
+        'C': 99, 
+        'D': 1, 
+        'E': 7, 
+        'G': 0, 
+    } 
+    return H_dist.get(n, float('inf'))  # Return infinity if node is not in H_dist
+
+# Describe the graph
 Graph_nodes = { 
-'A': [('B', 2), ('E', 3)], 
-'B': [('C', 1),('G', 9)], 
-'C': None, 
-'E': [('D', 6)], 
-'D': [('G', 1)], 
- 
+    'A': [('B', 2), ('E', 3)], 
+    'B': [('C', 1), ('G', 9)], 
+    'C': [], 
+    'E': [('D', 6)], 
+    'D': [('G', 1)], 
 } 
-aStarAlgo('A', 'G') 
- 
+
+# Run the A* Algorithm
+aStarAlgo('A', 'G')
  
  
  
