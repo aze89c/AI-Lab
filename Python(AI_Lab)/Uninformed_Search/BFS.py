b18750e4fdf2bdef883bf8c4c6b082115ac0d603
aze89c <azizbharmal07@gmail.com>
2025-05-10 17:45:52 +0530

    Update BFS.py

--- Python(AI_Lab)/Uninformed_Search/BFS.py
+++ Python(AI_Lab)/Uninformed_Search/BFS.py
@@ -24,42 +24,50 @@ o BFS needs lots of time if the solution is far away from the root node.
 # 1a. Write a Program to implement Uninformed search technique: BFS
 from collections import defaultdict
 # This class represents a graph using adjacency list representation
+from collections import defaultdict
+
+# This class represents a graph using adjacency list representation
 class Graph:
-# Constructor
+    # Constructor
     def __init__(self):
-# default dictionary to store graph
-     self.graph = defaultdict(list)
-# function to add an edge to graph
-    def addEdge(self,u,v):
-        self.graph[u].append((v))
-        self.graph[v].append((u))
+        # default dictionary to store graph
+        self.graph = defaultdict(list)
+
+    # function to add an edge to graph
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
     def dispg(self):
-        print(self.graph.items())
-#Function to print a BFS of graph
-    def BFS(self, s,goal):
-# Mark all the vertices as not visited
-        visited = [False] * (len(self.graph))
-# Create a queue for BFS
+        for key, value in self.graph.items():
+            print(f"{key}: {value}")
+
+    # Function to print a BFS of graph
+    def BFS(self, s, goal):
+        # Mark all the vertices as not visited
+        visited = [False] * (max(self.graph) + 1)
+        # Create a queue for BFS
         frontier_q = []
-# Mark the source node as visited and enqueue it
+        # Mark the source node as visited and enqueue it
         frontier_q.append(s)
         visited[s] = True
+
         while frontier_q:
-# Dequeue a vertex from queue and print it
-            s = frontier_q.pop(0)
-            print (s, end = " ")
-        if(s == goal):
-            print("\n Goal found")
-        return
-# Get all adjacent vertices of the dequeued vertex s.
-#  # If adjacent hasnot beenvisited, then mark it visited and enqueue it
-        for i in self.graph[s]:
-            if visited[i] == False:
-                frontier_q.append(i)
-                visited[i] = True
-                print("\n Goal not found")
+            # Dequeue a vertex from queue and print it
+            current = frontier_q.pop(0)
+            print(current, end=" ")
+            if current == goal:
+                print("\nGoal found")
+                return
+            # Get all adjacent vertices of the dequeued vertex current.
+            # If adjacent has not been visited, then mark it visited and enqueue it
+            for neighbor in self.graph[current]:
+                if not visited[neighbor]:
+                    frontier_q.append(neighbor)
+                    visited[neighbor] = True
+        print("\nGoal not found")
+
 # Driver code
-# Create a graph given in the above diagram
 g = Graph()
 g.addEdge(0,1)
 g.addEdge(0,3)
@@ -69,8 +77,8 @@ g.addEdge(3,4)
 g.addEdge(4,5)
 g.addEdge(3,5)
 g.dispg()
-print ("Following is Breadth First Traversal (starting from vertex 1)")
-g.BFS(1,4)
+print("Following is Breadth First Traversal (starting from vertex 1):")
+g.BFS(1, 4)
 
 '''
 OUTPUT:
